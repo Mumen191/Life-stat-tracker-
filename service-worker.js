b0d82bca9a7fe23ea4e73deaacb2dedf328a4416
MRIF <mumenrifath@gmail.com>
2026-07-18 17:15:20 -0400

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,5 +0,0 @@
-const C='life-rpg-working-v1';
-const A=['./','./index.html','./styles.css','./app.js','./manifest.json'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(A)))});
-self.addEventListener('activate',e=>{e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x))))]))});
-self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).then(r=>{const x=r.clone();caches.open(C).then(c=>c.put(e.request,x));return r}).catch(()=>caches.match(e.request)))});
